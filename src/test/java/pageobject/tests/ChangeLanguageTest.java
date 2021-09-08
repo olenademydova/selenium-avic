26e168bba0f6f32b8e98a9b8ddf99098978a4f8e
olenademydova <dmdv.lena@gmail.com>
2021-09-08 16:16:47 +0300

    Avic tests implemented

---
+++ src/test/java/pageobject/tests/ChangeLanguageTest.java
@@ -0,0 +1,17 @@
+package pageobject.tests;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class ChangeLanguageTest extends BaseTest {
+
+
+    @Test
+    public void checkLanguageChanging() {
+        getHomePage().changeLanguage();
+        assertEquals(getDriver().getCurrentUrl(), "https://avic.ua/ua");
+        assertEquals(getHomePage().getTextInSearchField(), "Пошук");
+    }
+
+}
